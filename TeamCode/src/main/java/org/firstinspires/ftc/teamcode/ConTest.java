05d6b7627de552f5cf9581375a5482cab991d317
snailatspace <mark.shtarkmanbor@gmail.com>
2021-02-19 18:32:03 +0800

    Giant commit to comlpy with proper formatting rules
    massive prettyification and added some features present in local project

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ConTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ConTest.java
@@ -3,65 +3,42 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 
-@TeleOp(name = "ConTest (Blocks to Java)", group = "")
+@TeleOp(name = "Controller Test: Commit Of The Year Edition", group = "")
 public class ConTest extends LinearOpMode {
-
-  private DcMotor FRmotor;
-  private DcMotor RRmotor;
-  private DcMotor FLmotor;
-  private DcMotor RLmotor;
-
-  /**
-   * This function is executed when this Op Mode is selected from the Driver Station.
-   */
   @Override
-  public void runOpMode() {
-    boolean controlMode;
-    double for_axis;
-    double strafe_axis;
-    double turn_axis=0;
-    FRmotor = hardwareMap.get(DcMotor.class, "FRmotor");
-    RRmotor = hardwareMap.get(DcMotor.class, "RRmotor");
-    FLmotor = hardwareMap.get(DcMotor.class, "FLmotor");
-    RLmotor = hardwareMap.get(DcMotor.class, "RLmotor");
-
-    // Reverse one of the drive motors.
-    // You will have to determine which motor to reverse for your robot.
-    // In this example, the right motor was reversed so that positive
-    // applied power makes it move the robot in the forward direction.
+  public void runOpMode() {                                                                         //Initialization phase
+    DcMotor FRmotor = hardwareMap.get(DcMotor.class, "FRmotor");                        //Hardware mapping and declaration of devices
+    DcMotor RRmotor = hardwareMap.get(DcMotor.class, "RRmotor");
+    DcMotor FLmotor = hardwareMap.get(DcMotor.class, "FLmotor");
+    DcMotor RLmotor = hardwareMap.get(DcMotor.class, "RLmotor");
+    DcMotor Worm = hardwareMap.get(DcMotor.class, "worm");
+    Servo Grabber = hardwareMap.get(Servo.class,"grabber");
     waitForStart();
-    if (opModeIsActive()) {
-      controlMode = false;
-      // Put run blocks here.
-      while (opModeIsActive()) {
-          for_axis = gamepad1.left_stick_y;
-          strafe_axis = gamepad1.left_stick_x;
-          turn_axis = gamepad1.right_stick_x;
-        // The Y axis of a joystick ranges from -1 in its topmost position
-        // to +1 in its bottommost position. We negate this value so that
-        // the topmost position corresponds to maximum forward power.
-        FRmotor.setPower(for_axis+strafe_axis*2.0+turn_axis*2.0);
-        RRmotor.setPower(for_axis-strafe_axis*2.0+turn_axis*2.0);
-        FLmotor.setPower(-for_axis+strafe_axis*2.0+turn_axis*2.0);
-        RLmotor.setPower(-for_axis-strafe_axis*2.0+turn_axis*2.0);
+    if (opModeIsActive()) {                                                                         //Pre-run phase
+      boolean grab = false;
+      while (opModeIsActive()) {                                                                    //Run phase
+        boolean prevgrab = grab;                                                                    //Data fetching
+        double for_axis = LogarithmifyInput(gamepad1.left_stick_y,2);
+        double strafe_axis = LogarithmifyInput(gamepad1.left_stick_x,2);
+        double turn_axis = LogarithmifyInput(gamepad1.right_stick_x,2);
+        double worm_axis = LogarithmifyInput(gamepad2.right_stick_y,2);
+        grab = gamepad2.right_bumper;
+        FRmotor.setPower(for_axis + strafe_axis + turn_axis);                                       //wheel motor movement
+        RRmotor.setPower(for_axis - strafe_axis + turn_axis);
+        FLmotor.setPower(-for_axis + strafe_axis + turn_axis);
+        RLmotor.setPower(-for_axis - strafe_axis + turn_axis);
+        Worm.setPower(worm_axis);
+        if(!prevgrab && grab) {                                                                     //arm servo movement
+          Grabber.setPosition(0.88-Grabber.getPosition());                                          //0.88=0.66(open state)+0.22(closed state)
+        }
       }
     }
   }
 
-  /**
-   * Describe this function...
-   */
-  private double convert(boolean bool) {
-    double ret;
-
-    if (bool) {
-      ret = 1;
-    } else {
-      ret = 0;
-    }
-    return ret;
+  public static double LogarithmifyInput(double input,int power) {
+    return Math.pow(input,power)*Math.signum(input);
   }
 }
+
