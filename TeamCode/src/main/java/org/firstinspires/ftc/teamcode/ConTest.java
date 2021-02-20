dbc5071bbc8a877db174e701b13c89b7c393d349
SnailAtSpace <52292975+SnailAtSpace@users.noreply.github.com>
2021-02-20 15:02:16 +0800

    ZeroPowerBehavior added to prevent motor drift

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ConTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ConTest.java
@@ -15,6 +15,10 @@ public class ConTest extends LinearOpMode {
     DcMotor RLmotor = hardwareMap.get(DcMotor.class, "RLmotor");
     DcMotor Worm = hardwareMap.get(DcMotor.class, "worm");
     Servo Grabber = hardwareMap.get(Servo.class,"grabber");
+    FRmotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    FLmotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    RRmotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    RLmotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
     waitForStart();
     if (opModeIsActive()) {                                                                         //Pre-run phase
       boolean grab = false;
