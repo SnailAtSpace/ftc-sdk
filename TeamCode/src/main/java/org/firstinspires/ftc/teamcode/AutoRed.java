dce05148809743458d46600e1596ce66a9f67284
snailatspace <mark.shtarkmanbor@gmail.com>
2021-03-20 10:41:01 +0800

    oasdfjoiosdfgaisfgaoosfgwfsgvfsgappihoufsvapiunbh

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoRed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoRed.java
@@ -6,7 +6,6 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
@@ -19,28 +18,30 @@ public class AutoRed extends LinearOpMode {
     OpenCvCamera webcam;
     BingusPipeline pipeline;
     Boolean ExecuteFlag;
-    DcMotor FRmotor, RRmotor, FLmotor, RLmotor, Worm, Flywheel, Collector;
-    Servo Grabber, Pushrod;
+    CommonValues commonValues;
+//    DcMotor FRmotor, RRmotor, FLmotor, RLmotor, Worm, Flywheel, Collector;
+//    Servo Grabber, Pushrod;
     public BingusPipeline.RandomizationFactor ringData;
     public ElapsedTime whenAreWe = new ElapsedTime(ElapsedTime.Resolution.MILLISECONDS);
     @SuppressLint("DefaultLocale")
     @Override
     public void runOpMode(){
-        FRmotor = hardwareMap.get(DcMotor.class, "FRmotor");                        //Hardware mapping and declaration of devices
-        RRmotor = hardwareMap.get(DcMotor.class, "RRmotor");
-        FLmotor = hardwareMap.get(DcMotor.class, "FLmotor");
-        RLmotor = hardwareMap.get(DcMotor.class, "RLmotor");
-        Flywheel = hardwareMap.get(DcMotor.class, "FWmotor");
-        Worm = hardwareMap.get(DcMotor.class, "Wmotor");
-        Grabber = hardwareMap.get(Servo.class,"Gservo");
-        Pushrod = hardwareMap.get(Servo.class,"Pservo");
-        Collector = hardwareMap.get(DcMotor.class,"Cmotor");
-        FRmotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        FLmotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        RRmotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        RLmotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        Flywheel.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
-        Worm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+//        FRmotor = hardwareMap.get(DcMotor.class, "FRmotor");                        //Hardware mapping and declaration of devices
+//        RRmotor = hardwareMap.get(DcMotor.class, "RRmotor");
+//        FLmotor = hardwareMap.get(DcMotor.class, "FLmotor");
+//        RLmotor = hardwareMap.get(DcMotor.class, "RLmotor");
+//        Flywheel = hardwareMap.get(DcMotor.class, "FWmotor");
+//        Worm = hardwareMap.get(DcMotor.class, "Wmotor");
+//        Grabber = hardwareMap.get(Servo.class,"Gservo");
+//        Pushrod = hardwareMap.get(Servo.class,"Pservo");
+//        Collector = hardwareMap.get(DcMotor.class,"Cmotor");
+//        FRmotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+//        FLmotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+//        RRmotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+//        RLmotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+//        Flywheel.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+//        Worm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        commonValues.Initialize(hardwareMap);
         //Flywheel.setDirection(DcMotorSimple.Direction.REVERSE);
         int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
         webcam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
@@ -54,8 +55,8 @@ public class AutoRed extends LinearOpMode {
         });
         telemetry.addLine("Waiting for start");
         telemetry.update();
-        FLmotor.setDirection(DcMotorSimple.Direction.REVERSE);
-        RLmotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        CommonValues.FLmotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        CommonValues.RLmotor.setDirection(DcMotorSimple.Direction.REVERSE);
         while((!isStarted())&&(!isStopRequested())){
             ringData=pipeline.getAnal();
             telemetry.addData("Best guess of ring amount: ",ringData);
@@ -74,14 +75,14 @@ public class AutoRed extends LinearOpMode {
             }
         }
         if(opModeIsActive()){
-            Grabber.scaleRange(0.16,0.66);
-            Pushrod.scaleRange(0.19,0.25);
-            Pushrod.setPosition(0);
+            CommonValues.Grabber.scaleRange(0.16,0.66);
+            CommonValues.Pushrod.scaleRange(0.19,0.25);
+            CommonValues.Pushrod.setPosition(0);
             whenAreWe.reset();
             ExecuteFlag=false;
             while(opModeIsActive()){
                 if(!ExecuteFlag) {
-                    Grabber.setPosition(0);
+                    CommonValues.Grabber.setPosition(0);
                     MoveWithEncoder(1845, 2);
                     TurnBySeconds(95,1);
                     LaunchSeveralRings(3);
@@ -105,20 +106,20 @@ public class AutoRed extends LinearOpMode {
                         MoveWithEncoder(600, 0);
                         MoveWithEncoder(400, 1);
                         DeployArm();
-                        Grabber.setPosition(1);
+                        CommonValues.Grabber.setPosition(1);
                         sleep(2000);
                         MoveWithEncoder(600, 2);
                     } else {
                         if (ringData == BingusPipeline.RandomizationFactor.ZERO) {
                             MoveWithEncoder(600, 3);
                             DeployArm();
-                            Grabber.setPosition(1);
+                            CommonValues.Grabber.setPosition(1);
                             sleep(2000);
                         } else {
                             MoveWithEncoder(600, 3);
                             MoveWithEncoder(1200, 0);
                             DeployArm();
-                            Grabber.setPosition(1);
+                            CommonValues.Grabber.setPosition(1);
                             sleep(2000);
                             MoveWithEncoder(1200, 2);
                         }
@@ -134,66 +135,66 @@ public class AutoRed extends LinearOpMode {
         //0=backward, 1=right, 2=forward, 3=left
         ElapsedTime localTime = new ElapsedTime(ElapsedTime.Resolution.MILLISECONDS);
         while(localTime.time()<=millis*1.135) { //what the fuck am i doing
-            RLmotor.setPower(Math.signum((direction-1)*2-1));
-            RRmotor.setPower(Math.signum(direction%3*2-1));
-            FLmotor.setPower(Math.signum(direction%3*2-1));
-            FRmotor.setPower(Math.signum((direction-1)*2-1));
+            CommonValues.RLmotor.setPower(Math.signum((direction-1)*2-1));
+            CommonValues.RRmotor.setPower(Math.signum(direction%3*2-1));
+            CommonValues.FLmotor.setPower(Math.signum(direction%3*2-1));
+            CommonValues.FRmotor.setPower(Math.signum((direction-1)*2-1));
         }
-        RLmotor.setPower(0);
-        RRmotor.setPower(0);
-        FLmotor.setPower(0);
-        FRmotor.setPower(0);
+        CommonValues.RLmotor.setPower(0);
+        CommonValues.RRmotor.setPower(0);
+        CommonValues.FLmotor.setPower(0);
+        CommonValues.FRmotor.setPower(0);
         sleep(250);
     }
     public void DeployArm(){
-        Worm.setPower(-1);
+        CommonValues.Worm.setPower(-1);
         sleep(2100);
-        Worm.setPower(0);
+        CommonValues.Worm.setPower(0);
     }
     public void MoveWithEncoder(int millis,int direction){
         //direction counted from 0, being backwards, counterclockwise
         //0=backward, 1=right, 2=forward, 3=left
-        FRmotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        FRmotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        FRmotor.setTargetPosition(millis);
-        FRmotor.setPower(Math.signum((direction-1)*2-1));
-        RLmotor.setPower(Math.signum((direction-1)*2-1));
-        RRmotor.setPower(Math.signum(direction%3*2-1));
-        FLmotor.setPower(Math.signum(direction%3*2-1));
-        FRmotor.setPower(0);
-        RLmotor.setPower(0);
-        RRmotor.setPower(0);
-        FLmotor.setPower(0);
+        CommonValues.FRmotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        CommonValues.FRmotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        CommonValues.FRmotor.setTargetPosition(millis);
+        CommonValues.FRmotor.setPower(Math.signum((direction-1)*2-1));
+        CommonValues.RLmotor.setPower(Math.signum((direction-1)*2-1));
+        CommonValues.RRmotor.setPower(Math.signum(direction%3*2-1));
+        CommonValues.FLmotor.setPower(Math.signum(direction%3*2-1));
+        CommonValues.FRmotor.setPower(0);
+        CommonValues.RLmotor.setPower(0);
+        CommonValues.RRmotor.setPower(0);
+        CommonValues.FLmotor.setPower(0);
         sleep(250);
     }
     public void LaunchSeveralRings(int amount){
         ElapsedTime localTime = new ElapsedTime(ElapsedTime.Resolution.MILLISECONDS);
-        Flywheel.setPower(1);
+        CommonValues.Flywheel.setPower(1);
         while (localTime.time() <= 3000) {}
-        Pushrod.setPosition(1);
+        CommonValues.Pushrod.setPosition(1);
         while (localTime.time() <= 3100) {}
-        Pushrod.setPosition(0);
+        CommonValues.Pushrod.setPosition(0);
         for(int i=0;i<=amount--;i++){
             localTime.reset();
             while (localTime.time() <= 2000) {}
-            Pushrod.setPosition(1);
+            CommonValues.Pushrod.setPosition(1);
             while (localTime.time() <= 2100) {}
-            Pushrod.setPosition(0);
+            CommonValues.Pushrod.setPosition(0);
         }
-        Flywheel.setPower(0);
+        CommonValues.Flywheel.setPower(0);
     }
     public void TurnBySeconds(int millis,int direction){
         ElapsedTime localTime = new ElapsedTime(ElapsedTime.Resolution.MILLISECONDS);
         while(localTime.time()<=millis) {
-            RLmotor.setPower(1-direction*2);
-            RRmotor.setPower(direction*2-1);
-            FLmotor.setPower(1-direction*2);
-            FRmotor.setPower(direction*2-1);
+            CommonValues.RLmotor.setPower(1-direction*2);
+            CommonValues.RRmotor.setPower(direction*2-1);
+            CommonValues.FLmotor.setPower(1-direction*2);
+            CommonValues.FRmotor.setPower(direction*2-1);
         }
-        RLmotor.setPower(0);
-        RRmotor.setPower(0);
-        FLmotor.setPower(0);
-        FRmotor.setPower(0);
+        CommonValues.RLmotor.setPower(0);
+        CommonValues.RRmotor.setPower(0);
+        CommonValues.FLmotor.setPower(0);
+        CommonValues.FRmotor.setPower(0);
         sleep(100);
     }
 }
