08e6d8a9a31d2dbae28f0f68fb3aa6d8695c44d8
SnailAtSpace <52292975+SnailAtSpace@users.noreply.github.com>
2022-11-08 14:35:54 +0800

    Code cleanup

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CommonOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CommonOpMode.java
@@ -1,11 +1,11 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
@@ -15,31 +15,39 @@ import org.openftc.easyopencv.OpenCvCameraFactory;
 import org.openftc.easyopencv.OpenCvCameraRotation;
 
 public abstract class CommonOpMode extends LinearOpMode {
-    public SampleMecanumDrive drive;
-    public DcMotorEx riserMotor;
+
+    // constants
+    final double restrictorCap = 1;
+    final double width = 380/25.4, length = 330/25.4, diag = Math.hypot(width,length);
+    final double hWidth = width/2, hLength = length/2,hDiag=diag/2, fieldHalf = 70.5;
+    long upperArmLimit=1030;
+
+    // webcam and rand-related information
     public OpenCvCamera webcam;
     public BingusPipeline pipeline;
     public BingusPipeline.RandomizationFactor duckPos = BingusPipeline.RandomizationFactor.UNDEFINED;
-    final double restrictorCap = 1;
-    double forward_axis, strafe_axis, turn_axis, riser_axis;
+
+    // actuators
+    public SampleMecanumDrive drive;
+    public DcMotorEx riserMotor;
+    public Servo riserServo;
+
+    // i/o
+    double forward_axis, strafe_axis, turn_axis, riser_axis; // analog inputs
+    boolean riserArm; // digital inputs
+    boolean pRiserArm; // last-loop digital inputs
+    double riserPos;
     double restrictor = restrictorCap;
-    boolean previousFreight = false;
-    int collector, previousCollector = 0;
-    boolean freight;
-    long upperArmLimit=1030;
-    final double maxCollPower = 1;
-    final double width = 330.29/25.4, length = 380.78/25.4, diag = Math.hypot(width,length);
-    final double hWidth = width/2, hLength = length/2,hDiag=diag/2, fieldHalf = 70.5;
-    public Pose2d startPoseRed = new Pose2d(10.5,-fieldHalf+hLength, Math.toRadians(270));
-    public Pose2d warehousePoseRed = new Pose2d(fieldHalf-hLength-20,-fieldHalf+hWidth,Math.toRadians(0));
-    public Pose2d defaultPoseRed = new Pose2d(9.5,-fieldHalf+hWidth,Math.toRadians(0));
+
     public void Initialize(HardwareMap hardwareMap, boolean isAuto) {
         drive = new SampleMecanumDrive(hardwareMap);
         riserMotor = (DcMotorEx) hardwareMap.get(DcMotor.class, "riserMotor");
+        riserServo = hardwareMap.get(Servo.class, "riserServo");
+
         riserMotor.setDirection(DcMotorSimple.Direction.FORWARD);
         riserMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
         if (isAuto) {
-            riserMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
             riserMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
             int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
             webcam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
@@ -61,6 +69,7 @@ public abstract class CommonOpMode extends LinearOpMode {
             telemetry.update();
         }
         else{
+            riserMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         }
     }
 
@@ -69,19 +78,7 @@ public abstract class CommonOpMode extends LinearOpMode {
         while (localTime.time() < millis && opModeIsActive() && !isStopRequested()){}
     }
 
-
     public static double logifyInput(double input, int power) {
         return Math.abs(Math.pow(input, power)) * Math.signum(input);
-    }
-
-    @Deprecated
-    public void ramIntoWall(boolean isRed){
-        Pose2d startPose = drive.getPoseEstimate();
-        drive.setWeightedDrivePower(new Pose2d(0, (isRed?-1:1)*0.2,0));
-        drive.update();
-        safeSleep(600);
-        drive.setWeightedDrivePower(new Pose2d(0,0,0));
-        drive.update();
-        drive.setPoseEstimate(startPose);
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GigachadTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GigachadTeleOp.java
@@ -15,81 +15,59 @@ public class GigachadTeleOp extends CommonOpMode {
     public void runOpMode(){
         Initialize(hardwareMap,false);
         drive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        riserMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        riserMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        double[] drivePose;
-//        try {
-//            File file = AppUtil.getInstance().getSettingsFile("LastPosition");
-//            drivePose = Arrays.stream(ReadWriteFile.readFile(file).split(" ")).mapToDouble(Double::parseDouble).toArray();
-//        }
-//        catch (Exception e){
-//            drivePose = new double[]{0,0,0};
-//        }
-//        drive.setPoseEstimate(new Pose2d(drivePose[0],drivePose[1],drivePose[2]));
-//        freightServo.setPosition(1);
+        riserServo.setPosition(1);
         drive.update();
         waitForStart();
         while (opModeIsActive()){
+
+            // LL VALUE ASSIGNMENT
+
             //INPUT GATHERING
             forward_axis = gamepad1.left_stick_y;
             strafe_axis = gamepad1.left_stick_x;
             turn_axis = 0.65*gamepad1.right_stick_x;
-//            collector = (gamepad2.dpad_up?1:0)-(gamepad2.dpad_down?1:0);
-//            freight = gamepad2.right_bumper;
-//            carousel_axis = gamepad2.left_stick_x;
-            riser_axis = -gamepad2.right_stick_y;
-//            double riserPos = riserMotor.getCurrentPosition();
+            riserArm = gamepad2.right_bumper;
+            riser_axis = gamepad2.right_stick_y;
+            riserPos = riserMotor.getCurrentPosition();
 
             //RISER SAFETY
-//            if(armButton.isPressed()){
+//            if(riserPos<10){
 //                restrictor = restrictorCap;
 //                riser_axis = Math.max(0,riser_axis);
-//                //freight = false; //we CAN deploy the freight holder when our arm is retracted
-//                riserMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-//                riserMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+//            }
+//            else if(riserPos>upperArmLimit){
+//                riser_axis = Math.min(0,riser_axis);
 //            }
 //            else{
-//                if(riserPos>upperArmLimit){
-//                    riser_axis = Math.min(0,riser_axis);
-//                }
-//                if(riserPos>50){
-//                    restrictor = 0.33;
-//                    if(collector!=0 || collectorMotor.getPower()>0) collector = -1;
-//                }
+//                restrictor = 0.33;
 //            }
 
+
             /* POWER APPLICATION: most likely already working as intended, do not touch! */
-            //collector power
-//            if(previousCollector!=collector && collector!=0){
-//                collectorMotor.setPower((collector*maxCollPower-Math.abs(collectorMotor.getPower())*Math.signum(Math.signum(collectorMotor.getPower())+collector)));
-//            }
-//            //freight holder position
-//            if(!previousFreight && freight){
-//                freightServo.setPosition(1-freightServo.getPosition());
-//            }
+
             //drive power
             drive.setWeightedDrivePower(new Pose2d(
                     -forward_axis*restrictor,
                     -strafe_axis*restrictor,
                     -turn_axis*restrictor
             ));
-            //miscellaneous power
-//            carouselMotor.setPower(carousel_axis);
-            riserMotor.setPower(riser_axis*0.66);
-//            //utility variable assignment for buttons
-//            previousFreight = freight;
-//            previousCollector = collector;
+            riserMotor.setPower(riser_axis);
+
+            //freight holder position
+            if(!pRiserArm && riserArm){
+                riserServo.setPosition(1-riserServo.getPosition());
+            }
+            pRiserArm = riserArm;
+
             //TELEMETRY
             telemetry.addData("Drive ticks: ", String.format("%1$d %2$d %3$d %4$d", drive.getWheelTicks().toArray()));
             telemetry.addData("RR Position: ", drive.getPoseEstimate().toString());
-//            if(restrictor == restrictorCap){
-//                telemetry.addData("Speed: ", "HIGH - ARM DISENGAGED, FULL SPEED");
-//            } else {
-//                telemetry.addData("Speed: ", "LOW - ARM ENGAGED, AVOID RAPID MOVEMENTS");
-//            }
-//            telemetry.addData("Riser position: ", riserPos);
-//            telemetry.addData("Button state: ",armButton.isPressed());
-//            telemetry.addData("Element: ", hasElement()?"YES":"NO");
+            if(restrictor == restrictorCap){
+                telemetry.addData("Speed: ", "HIGH - ARM DISENGAGED, FULL SPEED");
+            } else {
+                telemetry.addData("Speed: ", "LOW - ARM ENGAGED, AVOID RAPID MOVEMENTS");
+            }
+            telemetry.addData("Riser position: ", riserPos);
             telemetry.addData("Dead wheels: ", String.format("%.3f %.3f %.3f", ((StandardTrackingWheelLocalizer)drive.getLocalizer()).getWheelVelocities().toArray()));
             telemetry.update();
             drive.update();
